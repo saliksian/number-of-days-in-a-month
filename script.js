f2a39256e8d90fa2eda70c5d39ee586308cb77e3
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2021-04-22 16:20:02 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let month = parseInt(prompt("Enter month Number "));
+
+switch(month)
+{
+    case 1: 
+    console.log("january days: 31");
+        break;
+    case 2: 
+    console.log("February days: 28/29");
+        break;
+    case 3: 
+    console.log("March days: 31");
+        break;
+    case 4: 
+    console.log("April days: 30");
+        break;
+    case 5: 
+    console.log("May days: 31");
+        break;
+    case 6: 
+    console.log("June days: 30");
+        break;
+    case 7: 
+    console.log("July days: 31");
+        break;
+    case 8: 
+    console.log("August days: 31");
+        break;
+    case 9: 
+    console.log("September days: 30");
+        break;
+    case 10: 
+    console.log("October days: 31");
+        break;
+    case 11: 
+    console.log("November days: 30");
+        break;
+    case 12: 
+    console.log("December days: 31");
+        break;
+    default: 
+    console.log("Invalid input! Please enter month number between 1-12");
+
+}
